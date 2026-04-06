a26232e8631d375cb0335d17f6188f0b10de4d95
ve3 <kawewong@gmail.com>
2026-04-06 08:36:20 +0700

    Move assets files into folder, rename admin folder.

--- App/Controllers/Admin/Hooks/DashboardWidgets/DownloadStat.php
+++ App/Controllers/Admin/Hooks/DashboardWidgets/DownloadStat.php
@@ -30,7 +30,7 @@ if (!class_exists('\\RundizDownloads\\App\\Controllers\\Admin\\Hooks\\DashboardW
 
             wp_enqueue_style('rundiz-downloads-dashboard-widget-css', plugin_dir_url(RUNDIZDOWNLOADS_FILE) . 'assets/css/Admin/Hooks/DashboardWidgets/DownloadStat/dashboard-widget.css', ['rundiz-downloads-font-awesome5'], RUNDIZDOWNLOADS_VERSION);
 
-            wp_enqueue_script('rundiz-downloads-chart-js', plugin_dir_url(RUNDIZDOWNLOADS_FILE) . 'assets/js/vendor/Chart.bundle.min.js', [], '2.7.3', true);
+            wp_enqueue_script('rundiz-downloads-chart-js', plugin_dir_url(RUNDIZDOWNLOADS_FILE) . 'assets/vendor/chartjs/Chart.bundle.min.js', [], '2.7.3', true);
             wp_enqueue_script('rundiz-downloads-dashboard-widget-js', plugin_dir_url(RUNDIZDOWNLOADS_FILE) . 'assets/js/Admin/Hooks/DashboardWidgets/DownloadStat/dashboard-widget.js', ['jquery', 'rundiz-downloads-chart-js', 'wp-util'], RUNDIZDOWNLOADS_VERSION, true);
             wp_localize_script(
                 'rundiz-downloads-dashboard-widget-js',
